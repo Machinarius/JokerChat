0edfc55b3d8a0f2a90507e5c218a2b4336dc7e55
Germán Valencia <machinarius92@gmail.com>
2019-08-24 14:54:40 -0500

    Initial commit

---
+++ JokerChat.Endpoint/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace JokerChat.Endpoint.Models {
+  public class ErrorViewModel {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
---
+++ JokerChat.HubServer/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using JokerChat.HubServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace JokerChat.HubServer.Controllers {
+  public class HomeController : Controller {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger) {
+      _logger = logger;
+    }
+
+    public IActionResult Index() {
+      return View();
+    }
+
+    public IActionResult Privacy() {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
